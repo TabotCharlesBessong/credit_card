33fa414919ebe810bff18322bf6841ba11fcbc41
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-09-16 07:11:57 +0100

    implementing state management and thunks

--- credit_app/store/index.ts
+++ credit_app/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import cardReducer from './cardSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    card: cardReducer,
   },
 });
 
