f8a8678a80c96d337ff03537bbe989171df0b5a1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-09-16 07:11:09 +0100

    defining interfaces

---
+++ credit_app/constants/enums.ts
@@ -0,0 +1,12 @@
+export enum CardStatus {
+  PENDING = 'PENDING',
+  ACTIVE = 'ACTIVE',
+  BLOCKED = 'BLOCKED',
+}
+
+export enum CardType {
+  VISA = 'VISA',
+  MASTERCARD = 'MASTERCARD',
+  AMEX = 'AMEX',
+  DISCOVER = 'DISCOVER',
+}
